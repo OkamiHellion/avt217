ee22010e90e391e262511f94c3363a2d382b8966
SoA 1 <soa1@1020-14.mesa.gmu.edu>
2019-04-02 12:21:52 -0400

    update

--- Day20Notes/js/main.js
+++ Day20Notes/js/main.js
@@ -4,4 +4,16 @@ $(document).ready(function()  {
     $('#number5').css('color', 'white');
     $('#hat5').toggleClass('black grey');
   });
+  $('#number4').hover(function()  {
+    $('#number4').css('color', 'black');
+  });
+  $('#number3').click(function()  {
+
+  });
+  $('#number2').click(function()  {
+
+  });
+  $('#number1').click(function()  {
+
+  });
 });
