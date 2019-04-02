fac48455a77127fec942d32bd39eac285399ee53
SoA 1 <soa1@1020-14.mesa.gmu.edu>
2019-04-02 11:44:20 -0400

    p2-update

---
+++ Day20Notes/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function()  {
+  // $('h1').css('color', 'white');
+  $('#number5').click(function()  {
+    $('#number5').css('color', 'white');
+    $('#hat5').toggleClass('black grey');
+  });
+});
